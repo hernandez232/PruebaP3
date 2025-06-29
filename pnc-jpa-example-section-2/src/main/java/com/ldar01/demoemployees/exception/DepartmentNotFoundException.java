a2e064bf66c565bdbbf345ee032e68a3d0f7802c
Andrea Peña <00075919@uca.edu.sv>
2025-06-29 08:53:16 -0600

    delete: project files

--- pnc-jpa-example-section-2/src/main/java/com/ldar01/demoemployees/exception/DepartmentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ldar01.demoemployees.exception;
-
-public class DepartmentNotFoundException extends RuntimeException{
-    public DepartmentNotFoundException(String message) {
-        super(message);
-    }
-}
--- pnc-jpa-example-section-2/src/main/java/com/ldar01/demoemployees/service/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.ldar01.demoemployees.service;
-
-import com.ldar01.demoemployees.dto.request.LoginRequest;
-
-public interface AuthService {
-    String login(LoginRequest loginRequest);
-}
